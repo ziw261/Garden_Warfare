c809af31d2df7374c3862e7836778df8939c37b7
ziw261 <ziw261@ucsd.edu>
2019-07-03 15:34:49 -0700

    WIP
    WIP

---
+++ Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class AttackerSpawner : MonoBehaviour {
+
+    [SerializeField] private float minSpawnDelay = 1f;
+    [SerializeField] private float maxSpawnDelay = 5f;
+    [SerializeField] private Attacker attackerPrefab;
+    
+    bool spawn = true;
+    
+    IEnumerator Start() {
+        while (spawn) {
+            yield return new WaitForSeconds(Random.Range(minSpawnDelay, maxSpawnDelay));
+            SpawnAttacker();
+        }
+    }
+
+    private void SpawnAttacker() {
+        Instantiate(attackerPrefab, transform.position, transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
